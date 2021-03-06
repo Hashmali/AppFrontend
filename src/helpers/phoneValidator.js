7dc1c94174c2ff29b18c50b85e60697b3c0633b1
SgnvJaber <sgnvunleashed5@gmail.com>
2021-03-06 17:48:06 +0200

    changes

--- src/helpers/phoneValidator.js
+++
@@ -1,6 +0,0 @@
-export function phoneValidator(phone) {
-  const re = /^[0][5][0|2|3|4|5|6|9]{1}[-]{0,1}[0-9]{7}$/
-  if (!phone || phone.length <= 0) return "Phone can't be empty."
-  if (!re.test(phone)) return 'Ooops! We need a valid phone number.'
-  return ''
-}
