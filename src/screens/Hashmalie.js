61681466193bef6c8db97f6e3c3c9e2377b5b137
SgnvJaber <sgnvunleashed5@gmail.com>
2021-03-27 21:43:10 +0300

    Fixed Navigation issues and added Edit Profile Page

--- src/screens/Hashmalie.js
+++ src/screens/Hashmalie.js
@@ -1,89 +1,109 @@
- import React from 'react'
-import { Text,View} from 'react-native';
+import React from 'react'
+import { Text, View } from 'react-native'
 //import Background1 from '../components/Background'
 import Button from '../components/Button'
 import BackButton from '../components/BackButton'
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
-import { MaterialIcons,MaterialCommunityIcons } from '@expo/vector-icons';
-import {Ionicons, AntDesign,FontAwesome } from '@expo/vector-icons'; 
-import { NavigationContainer } from '@react-navigation/native';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs'
+import { MaterialIcons, MaterialCommunityIcons } from '@expo/vector-icons'
+import { Ionicons, AntDesign, FontAwesome } from '@expo/vector-icons'
+import { NavigationContainer } from '@react-navigation/native'
 import Profile from './Profile'
+import EditProfile from './EditProfile'
+
 import About from './About'
 import Reports from './Reports'
 import CreateReport from './CreateReport'
 import ViewReport from './ViewReport'
 import Tasks from './Tasks'
 import { createStackNavigator } from '@react-navigation/stack'
-const Tab = createMaterialBottomTabNavigator();
-const ReportStack=createStackNavigator()
+const Tab = createMaterialBottomTabNavigator()
+const ReportStack = createStackNavigator()
+const ProfileStack = createStackNavigator()
 
-const ReportStackScreen=()=>(
-  <ReportStack.Navigator  >
-  <ReportStack.Screen name="Reports" component={Reports}   options={{
-    headerShown: false
-  }}
-  
-  />
-  <ReportStack.Screen name="CreateReport" component={CreateReport}/>
-  <ReportStack.Screen name="ViewReport" component={ViewReport} options={{
-    headerShown: false}}/>
-
-  
+const ReportStackScreen = () => (
+  <ReportStack.Navigator>
+    <ReportStack.Screen
+      name="Reports"
+      component={Reports}
+      options={{
+        headerShown: false,
+      }}
+    />
+    <ReportStack.Screen name="CreateReport" component={CreateReport} />
+    <ReportStack.Screen
+      name="ViewReport"
+      component={ViewReport}
+      options={{
+        headerShown: false,
+      }}
+    />
   </ReportStack.Navigator>
-
 )
 
+const ProfileStackScreen = (props) => (
+  <ProfileStack.Navigator>
+    <ProfileStack.Screen
+      name="Profile"
+      children={() => <Profile toke={props.toke} id={props.id} />}
+      options={{
+        headerShown: false,
+      }}
+    />
+    <ProfileStack.Screen
+      name="EditProfile"
+      component={EditProfile}
+      options={{
+        headerShown: false,
+      }}
+    />
+  </ProfileStack.Navigator>
+)
 
-
-
-function MyTabs({navigation,toke,id}) {
-
-
+function MyTabs({ toke, id }) {
   return (
-		<Tab.Navigator   
-		    initialRouteName="About"
-        shifting={true}
-        labeled={true}
-        sceneAnimationEnabled={false}
-        activeColor="white"
-        inactiveColor="white"
-        screenOptions={{
+    <Tab.Navigator
+      initialRouteName="About"
+      shifting={true}
+      labeled={true}
+      sceneAnimationEnabled={false}
+      activeColor="white"
+      inactiveColor="white"
+      screenOptions={{
         headerTitleStyle: {
-        fontWeight: 'bold',
-         color: 'white',
-          },
-          headerLeft:null,
-          headerStyle:{
-            backgroundColor:'black',
-          }
-         // headerShown: false,
-        }}
-        barStyle={{ backgroundColor: 'black' }}
+          fontWeight: 'bold',
+          color: 'white',
+        },
+        headerLeft: null,
+        headerStyle: {
+          backgroundColor: 'black',
+        },
+        // headerShown: false,
+      }}
+      barStyle={{ backgroundColor: 'black' }}
     >
-	<Tab.Screen
-	name="About"
-  children={()=><About toke={toke} id={id}/>}
-
-	options={{
-	  tabBarLabel: 'Home',
-	  tabBarIcon: ({ color }) => (
-		<FontAwesome name="h-square" size={26} color="white" />
-	  ),
-	}}
-  />
-			<Tab.Screen
-        name="Profile"
-        children={()=><Profile toke={toke} id={id}/>}
+      <Tab.Screen
+        name="About"
+        children={() => <About toke={toke} id={id} />}
         options={{
-          tabBarLabel: 'Profile',
+          tabBarLabel: 'Home',
           tabBarIcon: ({ color }) => (
-			<MaterialIcons name="person-pin" size={28} color="white" />
+            <FontAwesome name="h-square" size={26} color="white" />
           ),
         }}
       />
-	  <Tab.Screen
+      <Tab.Screen
+        name="Profile"
+        children={() => <ProfileStackScreen toke={toke} id={id} />}
+        options={{
+          tabBarLabel: 'Profile',
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons name="person-pin" size={28} color="white" />
+          ),
+        }}
+      />
+      <Tab.Screen
         name="Reports"
-        children={()=><ReportStackScreen toke={toke} id={id}/>}
+        children={() => <ReportStackScreen toke={toke} id={id} />}
         options={{
           tabBarLabel: 'Reports',
           tabBarIcon: ({ color }) => (
@@ -91,10 +111,9 @@ function MyTabs({navigation,toke,id}) {
           ),
         }}
       />
-	  <Tab.Screen
+      <Tab.Screen
         name="Tasks"
-        children={()=><Tasks toke={toke} id={id}/>}
-
+        children={() => <Tasks toke={toke} id={id} />}
         options={{
           tabBarLabel: 'Tasks',
           tabBarIcon: ({ color }) => (
@@ -102,21 +121,18 @@ function MyTabs({navigation,toke,id}) {
           ),
         }}
       />
-		</Tab.Navigator>
-	);
+    </Tab.Navigator>
+  )
 }
 
-const Hashmalie = ({route, navigation }) => {
-  const {toke,id}=route.params
+const Hashmalie = ({ route, navigation }) => {
+  const { toke, id } = route.params
 
   return (
-   <NavigationContainer independent={true} >
-      
-    <MyTabs toke={toke} id={id}/>
-  </NavigationContainer>
+    <NavigationContainer independent={true}>
+      <MyTabs toke={toke} id={id} />
+    </NavigationContainer>
   )
 }
 
 export default Hashmalie
-
-
--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,156 +1,134 @@
-import { format } from 'prettier';
-import React,{useState,useEffect} from 'react';
-import { Text,View,StyleSheet,Image,ActivityIndicator} from 'react-native';
+import { format } from 'prettier'
+import React, { useState, useEffect } from 'react'
+import { Text, View, StyleSheet, Image, ActivityIndicator } from 'react-native'
 import Background from '../components/Background'
-import {LinearGradient} from 'expo-linear-gradient'
-import {Title,Card,Button} from "react-native-paper"
-import {MaterialIcons} from "@expo/vector-icons"
+import { LinearGradient } from 'expo-linear-gradient'
+import { Title, Card, Button } from 'react-native-paper'
+import { MaterialIcons } from '@expo/vector-icons'
+import { useNavigation } from '@react-navigation/native'
+
 const Profile = (props) => {
-	useEffect(()=>{
-		fetchItems();
-	  },[]);
-	var toke="Token " + props.toke+" "
-	var url='https://hashmali-backend.herokuapp.com/api/worker/'+props.id+'/view/'
-	var url='https://hashmali-backend.herokuapp.com/api/worker/'+props.id+'/view/'
-	const requestOptions =
- 	{
-  		method: 'GET',
-  		headers: { 'Content-Type': 'application/json',
-  		'Authorization' : toke,}
-	};
-	console.log(JSON.stringify(url))
-		const [items,setItems]=useState([])
-		const [status,setStatus]=useState("")
-		const fetchItems= async ()=>{
-		const data=await fetch(url,requestOptions).catch(error=>console.error(error));
-			console.log(JSON.stringify(data))
+  useEffect(() => {
+    fetchItems()
+  }, [])
+  var toke = 'Token ' + props.toke + ' '
+  var url =
+    'https://hashmali-backend.herokuapp.com/api/worker/' + props.id + '/view/'
+  var url =
+    'https://hashmali-backend.herokuapp.com/api/worker/' + props.id + '/view/'
+  const navigation = useNavigation()
+  const requestOptions = {
+    method: 'GET',
+    headers: { 'Content-Type': 'application/json', Authorization: toke },
+  }
+  console.log(JSON.stringify(url))
+  const [items, setItems] = useState([])
+  const [status, setStatus] = useState('')
+  const fetchItems = async () => {
+    const data = await fetch(url, requestOptions).catch((error) =>
+      console.error(error)
+    )
+    console.log(JSON.stringify(data))
 
-		setStatus(data.status)
-		const items=await data.json();
-		setItems(items);
-		};
-		if(status=="200"){
-			return (
-			<View style={StyleSheet.root}>
-			<LinearGradient
-			colors={["#2e2e2e","#2e2e2e"]}
-			style={{height:"20%"}}
-			>
-			</LinearGradient>
-							
-			<View style={{alignItems:"center"}}>
-				<Image
-					source={{uri: items.image,}}
-					style={styles.pic}
-					/>
-				</View>
-				<View style={{alignItems:"center"}}>
-				<Title>My Profile</Title>
-				</View>
+    setStatus(data.status)
+    const items = await data.json()
+    setItems(items)
+  }
+  if (status == '200') {
+    return (
+      <View style={StyleSheet.root}>
+        <LinearGradient
+          colors={['#2e2e2e', '#2e2e2e']}
+          style={{ height: '20%' }}
+        ></LinearGradient>
 
+        <View style={{ alignItems: 'center' }}>
+          <Image source={{ uri: items.image }} style={styles.pic} />
+        </View>
+        <View style={{ alignItems: 'center' }}>
+          <Title>My Profile</Title>
+        </View>
 
-				<Card style={styles.myCard}>
-					<View style={styles.cardContent}>
-					<MaterialIcons name="account-box" size={32} color ="black"/>
-					<Text style={styles.myText}>{items.second_name+" "+items.first_name}</Text>
-					</View>
-				</Card>
+        <Card style={styles.myCard}>
+          <View style={styles.cardContent}>
+            <MaterialIcons name="account-box" size={32} color="black" />
+            <Text style={styles.myText}>
+              {items.second_name + ' ' + items.first_name}
+            </Text>
+          </View>
+        </Card>
 
-				<Card style={styles.myCard}>
-					<View style={styles.cardContent}>
-					<MaterialIcons name="email" size={32} color ="black"/>
-					<Text style={styles.myText}>{items.email}</Text>
-					</View>
-				</Card>
-				<Card style={styles.myCard}>
-					<View style={styles.cardContent}>
-					<MaterialIcons name="phone" size={32} color ="black"/>
-					<Text style={styles.myText}>{items.phone}</Text>
-					</View>
-				</Card>
-				<Card style={styles.myCard}>
-					<View style={styles.cardContent}>
-					<MaterialIcons name="home" size={24} color="black" />
-					<Text style={styles.myText}>{items.address}</Text>
-					</View>
-				</Card>
+        <Card style={styles.myCard}>
+          <View style={styles.cardContent}>
+            <MaterialIcons name="email" size={32} color="black" />
+            <Text style={styles.myText}>{items.email}</Text>
+          </View>
+        </Card>
+        <Card style={styles.myCard}>
+          <View style={styles.cardContent}>
+            <MaterialIcons name="phone" size={32} color="black" />
+            <Text style={styles.myText}>{items.phone}</Text>
+          </View>
+        </Card>
+        <Card style={styles.myCard}>
+          <View style={styles.cardContent}>
+            <MaterialIcons name="home" size={24} color="black" />
+            <Text style={styles.myText}>{items.address}</Text>
+          </View>
+        </Card>
 
-
-
-
-
-				<View style={{flexDirection:"row",padding:10,}}>
-				<Button
-				icon="account-edit"
-				mode="contained"
-				onPress={()=>console.log("pressed")}>
-					Edit
-				</Button>
-	
-				
-				</View>			
-	
-
-
-	
-				</View>
-			);
-		}
-	else{
-	return (
-		<View style={[styles.container, styles.horizontal]}>
-		<ActivityIndicator />
-		<ActivityIndicator size="large" color="black" />
-	  </View>
-	);
-}
+        <View style={{ flexDirection: 'row', padding: 10 }}>
+          <Button
+            icon="account-edit"
+            mode="contained"
+            onPress={() => navigation.navigate('EditProfile')}
+          >
+            Edit
+          </Button>
+        </View>
+      </View>
+    )
+  } else {
+    return (
+      <View style={[styles.container, styles.horizontal]}>
+        <ActivityIndicator />
+        <ActivityIndicator size="large" color="black" />
+      </View>
+    )
+  }
 }
 const styles = StyleSheet.create({
-	root:{
-		flex:1,
+  root: {
+    flex: 1,
+  },
+  myCard: {
+    margin: 3,
+  },
+  cardContent: {
+    flexDirection: 'row',
+    padding: 8,
+  },
+  myText: {
+    fontSize: 22,
+    marginTop: 3,
+    marginLeft: 5,
+  },
 
-	},
-	myCard:{
-		margin:3,
-	},
-	cardContent:{
-		flexDirection:"row",
-		padding:8,
-	},
-	myText:{
-		fontSize:22,
-		marginTop:3,
-		marginLeft:5,
+  pic: {
+    width: 140,
+    height: 140,
+    borderRadius: 140 / 2,
+    marginTop: -50,
+  },
 
-	},
-
-	pic: {
-		width: 140,
-		height: 140,
-		borderRadius: 140/2,
-		marginTop: -50,
-	},
-	 
-	container: {
-		flex: 1,
-		justifyContent: "center"
-	  },
-	  horizontal: {
-		flexDirection: "row",
-		justifyContent: "space-around",
-		padding: 10
-	  },
-
-
-
-
-
-
-
-
-
-
-
-
-		})
-export default Profile;
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  horizontal: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    padding: 10,
+  },
+})
+export default Profile
